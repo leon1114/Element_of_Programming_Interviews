57688b265606efaec1c69cf2589b6156fc0c0fa2
leonryu <leon1114@naver.com>
2020-09-19 21:45:07 +0900

    5.9 Enumerate All Primes To N

--- epi_judge_python/prime_sieve.py
+++ epi_judge_python/prime_sieve.py
@@ -5,8 +5,17 @@ from test_framework import generic_test
 
 # Given n, return all primes up to and including n.
 def generate_primes(n: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    # Sieve of Eratosthenes Implementations
+    primes = []
+    is_prime = [1] * (n+1)
+    for i in range(2, n+1):
+        if is_prime[i] == 0:
+            continue
+        primes.append(i)
+        for j in range(i + i, n+1, i):
+            is_prime[j] = 0
+
+    return primes
 
 
 if __name__ == '__main__':
